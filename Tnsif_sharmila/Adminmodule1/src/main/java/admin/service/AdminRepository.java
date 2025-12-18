6d6e2f9d89232b420fa0e1c43b0b7e6a1ae40847
Sharmila1609 <isharmila1604@gmail.com>
2025-12-18 21:55:45 +0530

    Add files via upload

---
+++ Tnsif_sharmila/Adminmodule1/src/main/java/admin/service/AdminRepository.java
@@ -0,0 +1,8 @@
+package admin.service;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AdminRepository extends JpaRepository<Admin, Long> {
+}
